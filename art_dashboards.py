69a36331b8d91cbf804bb2a3baaba92b1a8321d8
Alexandru0707 <alex_boys30@yahoo.com>
2018-10-05 16:10:40 +0100

    Connetct to MongoDB

--- art_dashboards.py
+++ art_dashboards.py
@@ -3,13 +3,13 @@ from flask import Flask
 from flask import render_template
 from pymongo import MongoClient
 import json
-
+import os
 
 app = Flask(__name__)
 
-MONGODB_HOST = 'localhost'
-MONGODB_PORT = 27017
-DBS_NAME = 'my_db'
+
+MONGO_URI = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+DBS_NAME = os.getenv('MONGO_DB_NAME', 'donorsUSA')
 COLLECTION_NAME = 'paintings'
 
 
@@ -67,7 +67,7 @@ def facebook_projects():
 
     # Open a connection to MongoDB using a with statement such that the
     # connection will be closed as soon as we exit the with statement
-    with MongoClient(MONGODB_HOST, MONGODB_PORT) as conn:
+    with MongoClient(MONGO_URI) as conn:
         # Define which collection we wish to access
         collection = conn[DBS_NAME][COLLECTION_NAME]
         # Retrieve a result set only with the fields defined in FIELDS
